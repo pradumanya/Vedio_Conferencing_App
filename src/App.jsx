cc03d8c06800d76bc35a20d0d900b807e835993e
pradumanya <pradumanyaausekar2037@gmail.com>
2025-04-22 23:11:56 +0530

    React app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Vedioconferencing from './Vedioconferencing';
+
+function App() {
+  return (
+    <div>
+      <Vedioconferencing />
+    </div>
+  );
+}
+
+export default App;
